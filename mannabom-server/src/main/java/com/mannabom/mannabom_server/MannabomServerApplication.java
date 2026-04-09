53acd596332c73e3fd341a79ed4cff13b488b2b4
juneon <juneon0830@naver.com>
2026-04-09 19:05:37 +0900

    chore: mannabom-server 초기 세팅 및 환경설정 파일 추가

---
+++ mannabom-server/src/main/java/com/mannabom/mannabom_server/MannabomServerApplication.java
@@ -0,0 +1,13 @@
+package com.mannabom.mannabom_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MannabomServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MannabomServerApplication.class, args);
+	}
+
+}
